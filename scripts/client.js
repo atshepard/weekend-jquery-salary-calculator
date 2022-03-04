36c89862f26fd0fec91e8db412faa1fb5a6f7c6f
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-03-04 17:01:39 -0600

    added grabInfo function and click listener to client.js

--- scripts/client.js
+++ scripts/client.js
@@ -2,15 +2,33 @@ $(readyNow);
 
 function readyNow() {
 
+$('#submitBtn').on('click', grabInfo);
+
 // Click Listeners go here: 
 // one for submit
 // one for delete (maybe if I get to stretch)
 
 }
 
+function grabInfo() {
+//grabs info from the form
+let employeeFN = $('#employeeFN').val()
+let employeeLN = $('#employeeLN').val()
+let employeeID = $('#employeeID').val()
+let jobTitle = $('#jobTitle').val()
+let annualSalary = $('#annualSalary').val()
+// console.log(employeeFN, employeeLN, employeeID, jobTitle, annualSalary);
+
+
+}
+
 function submitHandler() {
 //needs to take inputs and append them to the DOM
 //DOM location will be the <td></td>
+
+}
+
+function render() {
 
 }
 
